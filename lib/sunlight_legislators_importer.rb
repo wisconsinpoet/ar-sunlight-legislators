0f8d71419c1e4539c9b2722f9fe1657963c83c24
DBC Challenges <challenges@devbootcamp.com>
2013-05-30 16:12:19 -0600

    Update sunlight_legislators_importer.rb

--- lib/sunlight_legislators_importer.rb
+++ lib/sunlight_legislators_importer.rb
@@ -13,11 +13,11 @@ class SunlightLegislatorsImporter
   end
 end
 
-begin
-  raise ArgumentError, "you must supply a filename argument" unless ARGV.length == 1
-  SunlightLegislatorsImporter.import(ARGV[0])
-rescue ArgumentError => e
-  $stderr.puts "Usage: ruby sunlight_legislators_importer.rb <filename>"
-rescue NotImplementedError => e
-  $stderr.puts "You shouldn't be running this until you've modified it with your implementation!"
-end
+# begin
+#   raise ArgumentError, "you must supply a filename argument" unless ARGV.length == 1
+#   SunlightLegislatorsImporter.import(ARGV[0])
+# rescue ArgumentError => e
+#   $stderr.puts "Usage: ruby sunlight_legislators_importer.rb <filename>"
+# rescue NotImplementedError => e
+#   $stderr.puts "You shouldn't be running this until you've modified it with your implementation!"
+# end
